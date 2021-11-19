5d1bb5d9168a9d5ad2aca49c7c1b8f0b5a906bdf
xohoon <xohoonx2@gmail.com>
2021-11-19 18:51:56 +0900

    Querydsl setting

---
+++ src/main/java/xohoon/study/JPAquerydsl/JpAquerydslApplication.java
@@ -0,0 +1,13 @@
+package xohoon.study.JPAquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAquerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpAquerydslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xohoon/study/JPAquerydsl/JpAquerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package xohoon.study.JPAquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAquerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
